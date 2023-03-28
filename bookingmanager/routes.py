d6b2f042d191f4487db7b4d0c2b5feff239ccd36
James-Glennon <glennon.jjp91@gmail.com>
2023-03-28 08:17:27 +0000

    extend routes.py, add nav to base.html, create booking.html

--- bookingmanager/routes.py
+++ bookingmanager/routes.py
@@ -4,4 +4,14 @@ from bookingmanager import app, db
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("index.html")
+
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+
+@app.route("/booking")
+def booking():
+    return render_template("booking.html")
