6840f6c04db0f2c42314f59f620fb753851837cd
James-Glennon <glennon.jjp91@gmail.com>
2023-03-29 05:58:06 +0000

    add flask flash functionality to display to user.

--- bookingmanager/routes.py
+++ bookingmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import Flask, render_template, request, flash
 from bookingmanager import app, db
 
 
@@ -15,5 +15,8 @@ def menu():
 @app.route("/booking", methods=["GET", "POST"])
 def booking():
     if request.method == "POST":
-        print(request.form)
+        flash(
+            "{}, Thank you for your interest. Your booking request is being processed".format(request.form.get("name")))
+        flash("Your requested booking is {}".format(request.form.get("booking-date")))
+        flash("At {}".format(request.form.get("booking-time")))
     return render_template("booking.html", page_title="Booking")
--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 import os
 from bookingmanager import app
+from flask import Flask, render_template, request, flash
 
+if os.path.exists("env.py"):
+    import env
+
+app.secret_key = os.environ.get("SECRET_KEY")
 
 if __name__ == "__main__":
     app.run(
