ac7da0df171be2b35338327920790ec5aa13edbc
James-Glennon <glennon.jjp91@gmail.com>
2023-03-29 21:04:10 +0000

    add routes and nav link for staff page

--- bookingmanager/routes.py
+++ bookingmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, url_for
 from bookingmanager import app, db
 from bookingmanager.models import AllBookings, DayBookings
 
@@ -22,4 +22,13 @@ def booking():
         flash("Your requested booking is {}".format(request.form.get(
             "booking_date")))
         flash("At {}".format(request.form.get("booking_time")))
+        allbookings = AllBookings(name=request.form.get("name"))
+        db.session.add(allbookings)
+        db.session.commit()
+        return redirect(url_for("staff")) 
     return render_template("booking.html", page_title="Booking")
+
+
+@app.route("/staff")
+def staff():
+    return render_template("staff.html", page_title="Staff Portal")
